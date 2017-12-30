3c7fe95cf5aaaaab840b0cbb56369c97ecf0189b
James Burton <jb76769@gmail.com>
2017-12-29 19:30:40 -0600

    Initial Commit

---
+++ AVCam1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVCam1
+//
+//  Created by James Burton on 12/29/17.
+//  Copyright © 2017 SApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
